b4bda7960c6e450c2bddbf76d46d583bd3641041
Jose RF Junior <web2ajax@gmail.com>
2020-07-12 07:56:06 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# SpfingBoot-GRPC
+# SpfingBoot-GRPC
---
+++ grpc-client/src/main/java/br/grpc/client/GrpcClientApplication.java
@@ -0,0 +1,46 @@
+package br.grpc.client;
+
+import br.grpc.server.HelloRequest;
+import br.grpc.server.HelloResponse;
+import br.grpc.server.HelloServiceGrpc;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrpcClientApplication {
+
+	public static void main(String[] args) {
+		ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 9090)
+				.usePlaintext()
+				.build();
+
+		HelloServiceGrpc.HelloServiceBlockingStub stub
+				= HelloServiceGrpc.newBlockingStub(channel);
+
+		HelloResponse helloResponse = stub.hello(HelloRequest.newBuilder()
+				.setFirstName("Baeldung")
+				.setLastName("gRPC")
+				.build());
+
+		System.out.println(helloResponse);
+
+		channel.shutdown();
+
+		/*ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 8080)
+				.usePlaintext()
+				.build();
+
+		HelloServiceGrpc.HelloServiceBlockingStub stub
+				= HelloServiceGrpc.newBlockingStub(channel);
+
+		HelloResponse helloResponse = stub.hello(HelloRequest.newBuilder()
+				.setFirstName("Baeldung")
+				.setLastName("gRPC")
+				.build());
+
+		channel.shutdown();*/
+//		SpringApplication.run(GrpcClientApplication.class, args);
+	}
+
+}
